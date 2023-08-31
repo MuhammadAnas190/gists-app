07e5e82a4f74b6d66ecf8187c0fbdab87b8cf24a
Muhammad Anas Saud <Muhammad.anas190@gmail.com>
2023-09-01 03:37:40 +0500

    chore(config): lint issue and add prop-types

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-
-/* The ErrorBoundary class is a React component that catches and handles errors in its child components. */
+import PropTypes from 'prop-types';
+/* The ErrorBoundary class is a React component that catches and handles errors in
+its child components. */
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(
-    error
-  ) {
+  static getDerivedStateFromError() {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
@@ -17,24 +16,23 @@ class ErrorBoundary extends React.Component {
   componentDidCatch(error, errorInfo) {
     // You can also log the error to an error reporting service
     console.error('Error:', error);
-    console.error(
-      'Error Info:',
-      errorInfo
-    );
+    console.error('Error Info:', errorInfo);
   }
 
   render() {
-    if (this.state.hasError) {
+    const { hasError } = this.state;
+    const { children } = this.props;
+    if (hasError) {
       // render custom fallback UI
-      return (
-        <h1>
-          Oops! Something went wrong.
-        </h1>
-      );
+      return <h1>Oops! Something went wrong.</h1>;
     }
 
-    return this.props.children;
+    return children;
   }
 }
+
+ErrorBoundary.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default ErrorBoundary;
